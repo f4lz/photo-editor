8359d0de004ef238d00cdda8bf3fa204d1f2d696
Рогоза Игорь Михайлович <i.m.rogoza@stud.mospolytech.ru>
2024-02-08 17:33:21 +0300

    Инициализация проекта

---
+++ photo/.eslintrc.cjs
@@ -0,0 +1,33 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/no-setup-props-reactivity-loss": "error",
+    "vue/no-unused-refs": "error",
+    "vue/attributes-order": [
+      "error",
+      {
+        alphabetical: true,
+      },
+    ],
+    "vue/block-order": [
+      "error",
+      {
+        order: ["script", "template", "style"],
+      },
+    ],
+  }
+}
