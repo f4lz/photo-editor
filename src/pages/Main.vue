037792c7770265aee89fa0001adf3724d63e5a9e
Рогоза Игорь Михайлович <i.m.rogoza@stud.mospolytech.ru>
2024-02-08 17:40:32 +0300

    Роутер и 404 страница

---
+++ src/pages/Main.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h1>Main page</h1>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url))
     }
   }
 })
