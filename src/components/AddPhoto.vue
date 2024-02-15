5f2138f0963e94e505ca7950c5245437c9fac6dc
Igor Rogoza <krab336@mail.ru>
2024-02-15 10:51:57 +0300

    Рефакторинг кода

--- src/components/AddPhoto.vue
+++ src/components/AddPhoto.vue
@@ -1,12 +1,22 @@
 <script lang="ts" setup>
-import { Ref, onMounted, ref } from 'vue'
+import type { Ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
+/** `canvas` элемент для отрисовки и работы с изображением */
 const canvasElement: Ref<HTMLCanvasElement | undefined> = ref()
+
+/** Контекст `canvas` */
 const ctx: Ref<CanvasRenderingContext2D | undefined> = ref()
 
+/** Фотография для обработки */
 const image: HTMLImageElement = new Image()
 
-const changeSrc = (event) => {
+/** Координаты мыши */
+const x: Ref<Number | undefined> = ref()
+const y: Ref<Number | undefined> = ref()
+
+//TODO: изменить тип аргумента - `event`
+const onChangeImg = (event: any) => {
   const e = event.target
   if (e.files) {
     const file = e.files[0]
@@ -15,38 +25,46 @@ const changeSrc = (event) => {
   } else {
     image.src = e.value
   }
-  render()
+  onRenderImg()
 }
 
 onMounted(() => {
   ctx.value = canvasElement.value?.getContext('2d') || undefined
-  render()
+  onRenderImg()
 })
 
-const render = () => {
-  if (!ctx.value) {
-    return
-  }
-
+/** Отрисовка изображения */
+const onRenderImg = () => {
   image.onload = () => {
-    ctx.value.clearRect(
-      0,
-      0,
-      canvasElement.value.width,
-      canvasElement.value.height
-    )
-    ctx.value.drawImage(image, 0, 0)
+    ctx.value && canvasElement.value
+      ? (ctx.value.clearRect(
+          0,
+          0,
+          canvasElement.value.width,
+          canvasElement.value.height
+        ),
+        ctx.value.drawImage(image, 0, 0))
+      : console.error('Ошибка контекста или элемента страницы')
   }
+}
+
+//TODO: изменить тип аргумента - `event`
+const showImgDetailsInfo = (event: any) => {
+  x.value = event.x
+  y.value = event.y
 }
 </script>
 
 <template>
+  <canvas
+    ref="canvasElement"
+    height="500"
+    width="500"
+    @mousemove="showImgDetailsInfo" />
+  <div>X {{ x }} Y {{ y }}</div>
   <div>
     Загрузить фото
-    <input type="file" @change="changeSrc" />
-    <input type="text" placeholder="Ссылка на фото" @change="changeSrc" />
-  </div>
-  <div>
-    <canvas ref="canvasElement" width="500" height="500"></canvas>
+    <input type="file" @change="onChangeImg" />
+    <input placeholder="Ссылка на фото" type="text" @change="onChangeImg" />
   </div>
 </template>
